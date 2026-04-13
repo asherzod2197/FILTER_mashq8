4ad95126a67442fb2aaed0a675686057c4782998
Ataxanov <asherzod2197@gmail.com>
2026-04-13 12:52:29 +0500

    Add filter examples with various data types

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# 36-40
+roy = ["abc", "xyz"]
+print(list(filter(lambda x: "x" in x, roy)))
 
+roy = [14, 28, 42]
+print(list(filter(lambda x: x > 20, roy)))
+
+roy = ["one", "four", "six"]
+print(list(filter(lambda x: len(x) == 3, roy)))
+
+roy = [5, 15, 25]
+print(list(filter(lambda x: x < 20, roy)))
+
+roy = ["hello", "hi"]
+print(list(filter(lambda x: len(x) > 2, roy)))
